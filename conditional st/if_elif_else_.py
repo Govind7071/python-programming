988b2b6c778c98a15117bf4bb4cc09b96080fec4
Govind Yadav <yadavgovind707166@gmail.com>
2025-03-13 22:23:22 +0530

    initial

---
+++ conditional st/if_elif_else_.py
@@ -0,0 +1,12 @@
+#grade of a student
+marks=int(input(" enter the marks :"))
+if(marks>=90):
+    print("A grade")
+elif(marks>=80 and marks<90):
+    print("B grade")
+elif(marks>=70 and marks<80):
+    print("C grade")
+elif(marks>=60 and marks<70):
+    print("D grade")
+else:
+    print("E grade")
