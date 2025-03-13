a8f22b35a5f2241b52ec21140b7ea0a5b91521bd
Govind Yadav <yadavgovind707166@gmail.com>
2025-03-13 22:12:26 +0530

    code

--- taking_input.py
+++ taking_input.py
@@ -1,7 +1,6 @@
 #taking input from keyboard and printing it
-name=input("name: ")
-classs=int(input("class :"))
-marks=float(input("marks :"))
-print("my name is ",name)
-print("i read in class ",classs)
+name=input("enter your name: ")
+class_=int(input("enter your class_ :"))
+marks=float(input("enter marks :"))
+print("my name is ",name,"and i read in class ",class_)
 print("my highest mark is ",marks)
