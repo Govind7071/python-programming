ca0afa5ffeaaa791a5fb3c3bf2f136845ab73aac
Govind Yadav <yadavgovind707166@gmail.com>
2025-03-21 17:37:00 +0530

    initial commit

---
+++ conditional st/ternery_opr.py
@@ -0,0 +1,5 @@
+#clever if/ternery operator
+#<var>=(flase_val,true_val) [<codition>]
+age=int(input("enter the age :"))
+vote=("yes","no") [age <= 18]
+print("vote:",vote)
---
+++ conditional st/ternery_opt.py
@@ -0,0 +1,5 @@
+#single line\ternery operator
+#<var>=<statement1>if<condition>else<statement2>
+food=input("taste:")
+eat="no" if food=="sweet" else "yes"
+print(eat)
