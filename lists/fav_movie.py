01780fd8d71b011b59bf1805520bfa19349e28ac
Govind Yadav <yadavgovind707166@gmail.com>
2026-01-23 17:37:34 +0530

    initial

--- lists/fav_movie.py
+++ lists/fav_movie.py
@@ -6,3 +6,9 @@ movies.append(a)
 a=input("enter your favourite movie: ")
 movies.append(a)
 print(movies)
+
+
+#cube form 1 to 10 by list comprehension 
+cube =[num**3 for num in range(1,11)]
+print(cube)
+
